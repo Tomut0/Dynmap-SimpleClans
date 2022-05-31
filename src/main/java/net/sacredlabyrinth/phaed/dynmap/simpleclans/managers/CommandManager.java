28593da4631d112072dc8d94d018b27389be4024
Minat0_ <minat0_@outlook.com>
2022-05-31 18:33:57 +0300

    refactor: short method names, update onCommand method
    Signed-off-by: Minat0_ <minat0_@outlook.com>

--- src/main/java/net/sacredlabyrinth/phaed/dynmap/simpleclans/managers/CommandManager.java
+++ src/main/java/net/sacredlabyrinth/phaed/dynmap/simpleclans/managers/CommandManager.java
@@ -1,137 +1,126 @@
 package net.sacredlabyrinth.phaed.dynmap.simpleclans.managers;
 
-import java.util.Set;
-
+import net.sacredlabyrinth.phaed.dynmap.simpleclans.DynmapSimpleClans;
+import net.sacredlabyrinth.phaed.simpleclans.ClanPlayer;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-
-import net.sacredlabyrinth.phaed.dynmap.simpleclans.DynmapSimpleClans;
-import net.sacredlabyrinth.phaed.dynmap.simpleclans.entries.PlayerEntry;
-import net.sacredlabyrinth.phaed.simpleclans.ClanPlayer;
+import org.dynmap.markers.MarkerIcon;
 import org.jetbrains.annotations.NotNull;
 
-public class CommandManager implements CommandExecutor {
+import java.util.Set;
 
-	DynmapSimpleClans plugin;
+import static net.sacredlabyrinth.phaed.dynmap.simpleclans.DynmapSimpleClans.lang;
 
-	public CommandManager() {
-		plugin = DynmapSimpleClans.getInstance();
-	}
+public final class CommandManager implements CommandExecutor {
 
-	@Override
-	public boolean onCommand(@NotNull CommandSender sender, Command command, @NotNull String label, String[] args) {
-		if (command.getName().equals("clanmap")) {
-			if (args.length > 0) {
-				String cmd = args[0];
-				if (cmd.equalsIgnoreCase("help")) {
-					processHelpCommand(sender);
-					return true;
-				}
-				
-				if (cmd.equalsIgnoreCase("reload")) {
-					return processReloadCommand(sender);
-				}
+    private final @NotNull DynmapSimpleClans plugin;
 
-				if (sender instanceof Player) {
-					Player player = (Player) sender;
-					if (cmd.equalsIgnoreCase("seticon") && args.length == 2) {
-						return processSetIconCommand(player, args[1]);
-					}
-					if (cmd.equalsIgnoreCase("listicons")) {
-						return processListIconsCommand(player);
-					}
-					if (cmd.equalsIgnoreCase("toggle")) {
-						return processToggleCommand(player);
-					}
-				}
-			} else {
-				processHelpCommand(sender);
-				return true;
-			}
-		}
-		return false;
-	}
+    public CommandManager(@NotNull DynmapSimpleClans plugin) {
+        this.plugin = plugin;
+    }
 
-	private void processHelpCommand(@NotNull CommandSender sender) {
-		plugin.getConfig().getStringList("help-command").forEach(s -> sender.sendMessage(ChatColor.translateAlternateColorCodes('&', s)));
-	}
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, Command command, @NotNull String label, String[] args) {
+        if (command.getName().equals("clanmap")) {
+            if (args.length == 0 || (args.length == 1 && args[0].equalsIgnoreCase("help"))) {
+                help(sender);
+                return true;
+            }
 
-	private boolean processToggleCommand(Player player) {
-		if (player.hasPermission("simpleclans.map.toggle")) {
-			PlayerEntry entry = plugin.getPlayerManager().getEntry(player);
+            String cmd = args[0];
+            if (cmd.equalsIgnoreCase("reload")) {
+                return reload(sender);
+            }
 
-			if (entry.isVisible()) {
-				entry.setVisible(false);
-				player.sendMessage(plugin.getLang("not-visible"));
-			} else {
-				entry.setVisible(true);
-				player.sendMessage(plugin.getLang("visible"));
-			}
-		} else {
-			player.sendMessage(plugin.getLang("no-permission"));
-		}
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                if (cmd.equalsIgnoreCase("seticon") && args.length == 2) {
+                    return setIcon(player, args[1]);
+                }
+                if (cmd.equalsIgnoreCase("listicons")) {
+                    return listIcons(player);
+                }
+            }
+        }
+        return false;
+    }
 
-		return true;
-	}
+    /**
+     * Shows help command to the sender
+     */
+    private void help(@NotNull CommandSender sender) {
+        plugin.getConfig().getStringList("help-command").forEach(s ->
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', s)));
+    }
 
-	private boolean processListIconsCommand(Player player) {
-		if (!player.hasPermission("simpleclans.map.list")) {
-			player.sendMessage(plugin.getLang("no-permission"));
-			return true;
-		}
+    /**
+     * Retrieves the list of available icons
+     */
+    private boolean listIcons(@NotNull Player player) {
+        if (!player.hasPermission("simpleclans.map.list")) {
+            player.sendMessage(lang("no-permission"));
+            return true;
+        }
 
-		player.sendMessage(plugin.getLang("available-icons"));
-		Set<String> icons = plugin.getClanHomes().getIcons();
-		icons.forEach(icon -> player.sendMessage(plugin.getLang("icon-line").replace("@icon", icon)));
-		if (icons.isEmpty()) {
-			player.sendMessage(plugin.getLang("error-no-icons"));
-		}
+        player.sendMessage(lang("available-icons"));
 
-		return true;
-	}
+        Set<MarkerIcon> icons = plugin.getHomeLayer().getIconStorage().getIcons();
+        for (MarkerIcon icon : icons) {
+            String message = lang("icon-line").replace("{iconName}", icon.getMarkerIconLabel());
+            player.sendMessage(message);
+        }
 
-	private boolean processReloadCommand(@NotNull CommandSender sender) {
-		if (!sender.hasPermission("simpleclans.map.reload")) {
-			sender.sendMessage(plugin.getLang("no-permission"));
-			return true;
-		}
+        if (icons.isEmpty()) {
+            player.sendMessage(lang("error-no-icons"));
+        }
 
-		sender.sendMessage(plugin.getLang("reloading"));
-		plugin.cleanup();
-		PreferencesManager.loadPreferences();
-		plugin.reloadConfig();
-		plugin.activate();
+        return true;
+    }
 
-		return true;
-	}
+    /**
+     * Reloads the plugin
+     */
+    private boolean reload(@NotNull CommandSender sender) {
+        if (!sender.hasPermission("simpleclans.map.reload")) {
+            sender.sendMessage(lang("no-permission"));
+            return true;
+        }
 
-	private boolean processSetIconCommand(@NotNull Player player, @NotNull String icon) {
-		ClanPlayer cp = plugin.getClanManager().getClanPlayer(player);
-		if (cp == null || cp.getClan() == null) {
-			player.sendMessage(plugin.getLang("not-member"));
-			return true;
-		}
-		if (!player.hasPermission("simpleclans.map.seticon") || !cp.isLeader()) {
-			player.sendMessage(plugin.getLang("no-permission"));
-			return true;
-		}
+        sender.sendMessage(lang("reloading"));
+        plugin.cleanup();
+        Preferences.loadPreferences();
+        plugin.reload();
 
-		if (plugin.getClanHomes().getIcons().contains(icon.toLowerCase())) {
-			if (!player.hasPermission("simpleclans.map.icon.bypass") &&
-					!player.hasPermission("simpleclans.map.icon." + icon)) {
-				player.sendMessage(plugin.getLang("no-permission"));
-				return true;
-			}
-			PreferencesManager pm = new PreferencesManager(cp.getClan());
-			pm.setClanHomeIcon(icon);
-			player.sendMessage(plugin.getLang("icon-changed"));
-		} else {
-			player.sendMessage(plugin.getLang("icon-not-found"));
-		}
+        return true;
+    }
 
-		return true;
-	}
+    private boolean setIcon(@NotNull Player player, @NotNull String icon) {
+        ClanPlayer cp = plugin.getClanManager().getClanPlayer(player);
+        if (cp == null || cp.getClan() == null) {
+            player.sendMessage(lang("not-member"));
+            return true;
+        }
+        if (!player.hasPermission("simpleclans.map.seticon") || !cp.isLeader()) {
+            player.sendMessage(lang("no-permission"));
+            return true;
+        }
+
+        if (plugin.getHomeLayer().getIconStorage().has(icon.toLowerCase())) {
+            if (!player.hasPermission("simpleclans.map.icon.bypass") &&
+                    !player.hasPermission("simpleclans.map.icon." + icon)) {
+                player.sendMessage(lang("no-permission"));
+                return true;
+            }
+            Preferences pm = new Preferences(cp.getClan());
+            pm.setClanHomeIcon(icon);
+            player.sendMessage(lang("icon-changed"));
+        } else {
+            player.sendMessage(lang("icon-not-found"));
+        }
+
+        return true;
+    }
 }
