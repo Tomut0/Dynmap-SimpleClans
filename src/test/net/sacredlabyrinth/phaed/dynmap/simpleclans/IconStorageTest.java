7fa2c3b20d2008fc8e70a70457d2c29a16b9b49b
Minat0_ <minat0_@outlook.com>
2022-06-05 20:22:37 +0300

    test: test the possibility of retrieving a stream from plugin's resources
    Signed-off-by: Minat0_ <minat0_@outlook.com>

--- src/test/net/sacredlabyrinth/phaed/dynmap/simpleclans/IconStorageTest.java
+++ src/test/net/sacredlabyrinth/phaed/dynmap/simpleclans/IconStorageTest.java
@@ -1,25 +1,38 @@
 package net.sacredlabyrinth.phaed.dynmap.simpleclans;
 
 import org.dynmap.markers.MarkerAPI;
-import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.io.IOException;
+import java.io.InputStream;
+
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(MockitoJUnitRunner.class)
 public class IconStorageTest {
 
     @Mock
     MarkerAPI markerAPI;
+    @Mock
+    DynmapSimpleClans dynmapSimpleClans;
 
     @Test
     public void IsNotNull() {
-        try {
-            IconStorage iconStorage = new IconStorage("/images/clanhome", "clanhome.png", markerAPI);
-            Assert.assertNotNull(iconStorage);
+        IconStorage iconStorage = new IconStorage(dynmapSimpleClans,
+                "/images/clanhome", "clanhome", markerAPI);
+        assertNotNull(iconStorage);
+    }
+
+    @Test
+    @DisplayName("Tests the possibility of retrieving a stream from plugin's resources")
+    public void defaultIconNotNull() {
+        String path = "/images/clanhome/clanhome.png";
+        try (InputStream is = DynmapSimpleClans.class.getResourceAsStream(path)) {
+            assertNotNull(is);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
